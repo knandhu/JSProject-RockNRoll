feb7ca4b561c14b4a31e5e6f1e53058dbf5d4d5a
Nandhu <knandhu.27@gmail.com>
2020-01-27 20:34:51 -0800

    create sphere

---
+++ frontend/test1.js
@@ -0,0 +1,11 @@
+let canvas = document.getElementById("gamescreen");
+
+let ctx = canvas.getContext("2d");
+
+// ctx.clearRect(0, 0, 800, 600);
+ctx.fillStyle = '#f00';
+ctx.fillRect(20, 20, 100, 100);
+
+
+ctx.fillStyle = '#00f';
+ctx.fillRect(70, 70, 50, 50);
