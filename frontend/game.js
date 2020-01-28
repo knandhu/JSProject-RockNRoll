8fa23b199a111f9df8c9d32a85137481a39b4e19
Nandhu <knandhu.27@gmail.com>
2020-01-27 20:43:07 -0800

    create sphere separate js

---
+++ frontend/game.js
@@ -0,0 +1,35 @@
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+var sphereGeometry = new THREE.DodecahedronGeometry(0.2, 1);
+var sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xe5f2f2, shading: THREE.FlatShading })
+jumping = false;
+heroSphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+heroSphere.receiveShadow = true;
+heroSphere.castShadow = true;
+scene.add(heroSphere);
+heroSphere.position.y = 1.8;
+heroSphere.position.z = 4.8;
+currentLane = 0;
+heroSphere.position.x = currentLane;
+
+camera.position.z = 10;
+
+var animate = function () {
+    requestAnimationFrame(animate);
+
+    heroSphere.rotation.x += 0.01;
+    heroSphere.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+};
+
+animate();
